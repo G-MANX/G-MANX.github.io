12f14e508ff68652bde8294f1cf87f2a2e494862
G-MANX <nelsonazumah@gmail.com>
2020-12-16 04:57:45 +0000

    commit

--- website project/cars.js
+++ website project/cars.js
@@ -1,55 +1,56 @@
-let requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
-let request = new XMLHttpRequest();
-request.open("GET", requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function () {
-  let cardata = request.response;
-  showData(towndata);
-}
-  function showData(jsonObj) {
-    let town = jsonObj["towns"];
-    
-    for (var i = 0; i < car.length; i++) {
-      if (town[i].name == "Preston" || car[i].name == "Soda Springs" || car[i].name == "Fish Haven"){
-      let article = document.getElementById(car[i].name);
-      let myH2 = document.createElement("h2");
-      let para1 = document.createElement("p");
-      let para2 = document.createElement("p");
-      let para3 = document.createElement("p");
-      let para4 = document.createElement("p");
+const requestURL = 'https://github.com/G-MANX/G-MANX.github.io/blob/master/website%20project/.json';
 
-      myH2.textContent = car[i].name;
-      para1.textContent = "Motto: " + car[i].motto;
-      para2.textContent = "Year Founded: " + car[i].yearFounded;
-      para3.textContent = "Population: " + car[i].currentPopulation;
-      para4.textContent = "Annual Rain Fall: " + car[i].averageRainfall + " inches";
-
-      article.appendChild(myH2);
-      article.appendChild(para1);
-      article.appendChild(para2);
-      article.appendChild(para3);
-      article.appendChild(para4);
-
-      
-      }
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  
+    const cars = jsonObject['cars'];
+    var p, f, s;
+    for(let j=0; j < carss.length; j++){
+       if(carss[j].name == "Honda Pioneer.jpeg"){
+         p = j;
+       }
+       else if(cars[j].name == "Honda scooter.jpeg"){
+         f = j;
+       }
+       else if(cars[j].name == "Jeep wrangler.jpeg"){
+         s = j;
+       }
     }
-  }
+    var index = [p, f, s];
+    for (let i = 0; i < index.length; i++ ) {
+        let cars = document.createElement('div');
+        let data = document.createElement('div');
+        data.setAttribute("class","data");
+        car.setAttribute("id","town" + i);
+        car.setAttribute("class","town");
+        let h2 = document.createElement('h2');
+        h2.setAttribute("class","townheader")
+        let price = document.createElement('p');
+        motto.setAttribute("class","motto")
+        let yearfounded = document.createElement('p');
+        yearfounded.setAttribute("class","founded");
+        let pop = document.createElement('p');
+        pop.setAttribute("class", "pop");
+        let rain = document.createElement('p');
+        rain.setAttribute("class","rain");
+        let image = document.createElement('img');
 
-  function toggleMenu() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-  }
-
-var now = new Date();
-var days = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
-var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
-var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
-function fourdigits(number) {
-    return (number < 1000) ? number + 1900 : number;
-}
-today =  days[now.getDay()] + ", " +
-         date + " " +
-         months[now.getMonth()] + " " +
-         (fourdigits(now.getYear())) ;
-document.getElementById("currentdate").innerHTML = today;
-
+h2.textContent = towns[index[i]].name;
+car.textContent = "'" + cars[index[i]].car + "'";
+yearfounded.textContent = "Established: " + cars[index[i]].yearFounded; 
+pop.textContent = "Population: " + cars[index[i]].currentPopulation;
+rain.textContent = "Average Rainfall: " + cars[index[i]].averageRainfall; 
+image.setAttribute('src', cars[index[i]].photo);
+car.appendChild(data);
+data.appendChild(h2);
+data.appendChild(motto);
+data.appendChild(yearfounded);
+data.appendChild(pop);
+data.appendChild(rain);
+car.appendChild(image);
+document.querySelector('section.towns').appendChild(car);
+    }
+      });
